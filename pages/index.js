4a3584fbaa35af95de19ac0415d47cbed291e18c
Matheus <matheusramosbi.13@gmail.com>
2023-12-01 12:46:28 -0300

    change sentence

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Esta primeira versão é para minha namorada Júlia! Eu te amo meu amor, mal posso esperar para termos uma vida juntinhos no nosso cantinho, onde quer que este cantinho seja. 👀 🚐 ⛺</h1>
+    return <h1>Esta primeira versão é para minha namorada Júlia! Eu te amo meu amor, mal posso esperar para termos uma vida juntinhos no nosso cantinho, onde quer que este cantinho seja. 👀 🚐 ⛺ 🌍</h1>
 }
 
 export default Home;
