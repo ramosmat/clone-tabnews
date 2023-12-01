b7bcc2d6f1bfbb484b3719aebc88d49c2fc2aa6b
Matheus <matheusramosbi.13@gmail.com>
2023-12-01 13:08:45 -0300

    update message

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ function Home() {
         <html>
             <body>
                 <h1>Oi meu amor!</h1>
-                <p>Esta primeira versão é para minha namorada Júlia! Eu te amo meu amor, mal posso esperar para termos uma vida
-                    juntinhos no nosso cantinho, onde quer que este cantinho seja. 👀 🚐 ⛺ 🌍</p>
+                <p>Este é o primeiro desafio do curso.dev, com o objetivo de criar uma frase para uma pessoa e fazer a diferença no dia dela.
+                    E não podia ter outra pessoa ne?</p>
+                <p>Quero te dizer que mal posso esperar para termos uma vida juntinhos no nosso cantinho, onde quer que este cantinho seja. 👀 🚐 ⛺ 🌍</p>
                 <p>Te amo! 💖</p>
 
             </body>
