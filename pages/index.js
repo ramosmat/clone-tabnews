221aa5c3399b60c658774e2a3ea4623a4719499b
Matheus <matheusramosbi.13@gmail.com>
2023-12-07 11:25:32 -0300

    add scripts `lint:check` & `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import { Html } from "next/document";
 
 function Home() {
-    return (
-        <html>
-            <body>
-                <h1>Oi meu amor!</h1>
-                <p>Este é o primeiro desafio do curso.dev, com o objetivo de criar uma frase para uma pessoa e fazer a diferença no dia dela.
-                    E não podia ter outra pessoa ne?</p>
-                <p>Quero te dizer que mal posso esperar para termos uma vida juntinhos no nosso cantinho, onde quer que este cantinho seja. 👀 🚐 ⛺ 🌍</p>
-                <p>Te amo! 💖</p>
-
-            </body>
-        </html>
-    )
+  return (
+    <html>
+      <body>
+        <h1>Oi meu amor!</h1>
+        <p>
+          Este é o primeiro desafio do curso.dev, com o objetivo de criar uma
+          frase para uma pessoa e fazer a diferença no dia dela. E não podia ter
+          outra pessoa ne?
+        </p>
+        <p>
+          Quero te dizer que mal posso esperar para termos uma vida juntinhos no
+          nosso cantinho, onde quer que este cantinho seja. 👀 🚐 ⛺ 🌍
+        </p>
+        <p>Te amo! 💖</p>
+      </body>
+    </html>
+  );
 }
 
 export default Home;
