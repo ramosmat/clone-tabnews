adb36c9d39a5b217d9a92e53f1e4f021bcade75e
Matheus <matheusramosbi.13@gmail.com>
2023-12-23 15:07:50 -0300

    add tag head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import { Html } from "next/document";
+import { Head } from "next/head";
 
 function Home() {
   return (
     <html>
+      <head>
+        <meta charset="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <title>clone-tabnews</title>
+      </head>
       <body>
         <h1>Oi meu amor!</h1>
         <p>
